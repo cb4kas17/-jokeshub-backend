04af6774ea82d7af2461d242782601ab7c04ff58
Christian Bryan Portugal <christianbryneportugal@yahoo.com>
2022-02-28 19:57:31 +0800

    last

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 
 //middlewares
